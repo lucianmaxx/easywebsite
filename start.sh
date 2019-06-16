e70ed770667f6f1c5275a3287ce07062bb367c19
Ironozd <ironoziron@gmail.com>
2019-06-16 20:26:57 +0300

    unfinished

---
+++ start.sh
@@ -0,0 +1,19 @@
+banner(){
+
+printf "\e[1;77m▀█▀ █▀▀█ █▀▀█ █▀▀▄ █▀▀█ ▀▀█ █▀▀▄      \e[0m\n"
+printf "\e[1;77m▒█░ █▄▄▀ █░░█ █░░█ █░░█ ▄▀░ █░░█  \e[0m\n"
+printf "\e[1;77m▄█▄ ▀░▀▀ ▀▀▀▀ ▀░░▀ ▀▀▀▀ ▀▀▀ ▀▀▀░  \e[0m\n"
+printf "\n"
+printf "\e[1;93m       .:.:.\e[0m\e[1;77m WebHosting Tool coded by:  @Ironozd \e[0m\e[1;93m.:.:.\e[0m\n"
+printf "\n"
+printf "  \e[101m\e[1;77m:: Disclaimer: Developers assume no liability and are not    ::\e[0m\n"
+printf "  \e[101m\e[1;77m:: responsible for any misuse or damage caused by WebHosting Tool ::\e[0m\n"
+printf "\n"
+
+}
+
+start(){
+
+
+
+}
