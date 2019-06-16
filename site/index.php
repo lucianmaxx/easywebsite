91df16dc839c21fc8aacbf5c303d66aba6ab3fc4
Ironozd <ironoziron@gmail.com>
2019-06-16 22:19:10 +0300

    Update index.php

--- site/index.php
+++ site/index.php
@@ -3,7 +3,15 @@
 <title>YAY!</title>
 </head>
 <body>
+  <center>
 <h1>Hello World</h1>
-<h2>Files made by Ironozd</h2>
+<h2>Made by Ironozd</h2>
+  <br>
+    <br>
+    <h1>To Change this page;>
+      <h2>Delete this file and replace with your own index.html or index.php</h1>
+      <h2>If your website is on github, go to site folder and make <strong>git
+        clone <your_git_url> </strong></h2>
+  </center>
 </body>
 </html>
