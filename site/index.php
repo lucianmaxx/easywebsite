ac9caa3958ddc17591ef9d809d5b3a417ec8257e
Ironozd <ironoziron@gmail.com>
2019-06-16 22:19:34 +0300

    Update index.php

--- site/index.php
+++ site/index.php
@@ -11,7 +11,7 @@
     <h1>To Change this page;>
       <h2>Delete this file and replace with your own index.html or index.php</h2>
       <h2>If your website is on github, go to site folder and make <strong>git
-        clone <your_git_url> </strong></h2>
+        clone < your_git_url > </strong></h2>
   </center>
 </body>
 </html>
