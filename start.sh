2f1d98c13f3c3248712406b20fff7245920c6d7b
Ironozd <ironoziron@gmail.com>
2019-06-16 23:12:27 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -17,14 +17,18 @@ printf "\n"
 
 versioncheck(){
 
-    if [$version = curl https://raw.githubusercontent.com/Ironozd/easywebsite/master/version.txt] then
+    if [[$version = curl https://raw.githubusercontent.com/Ironozd/easywebsite/master/version.txt]]
+    then
+    
       start
       
       else
-            printf "  \e[101m\e[1;77m:: IMPORTANT UPDATE AVAILABLE    ::\e[0m\n"
+      
+  printf "  \e[101m\e[1;77m:: IMPORTANT UPDATE AVAILABLE    ::\e[0m\n"
   printf "\e[1;92m[\e[0m*\e[1;92m] Skipping in 5 seconds...\n"
   sleep 5
   start
+  
   fi
       
   
