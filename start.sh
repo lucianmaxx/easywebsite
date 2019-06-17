fe38ab1d33d9eefca8b49edf8edc9ace214dea67
Ironozd <ironoziron@gmail.com>
2019-06-17 15:09:57 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -16,7 +16,7 @@ printf "\n"
 
 versioncheck(){
 curversion=0.1
-version=$(curl https://raw.githubusercontent.com/Ironozd/easywebsite/master/version.txt 2> /dev/null > version)
+version=$(curl https://raw.githubusercontent.com/Ironozd/easywebsite/master/version.txt)
 
 if [ "$version" == "$curversion" ]; then
 start
